e5ee9198212b215128d9ae731993cfd4114ebd11
DeepKavar5105 <dkkavar7@gmail.com>
2024-07-27 22:28:34 +0530

    Add files via upload

---
+++ task 3/BankAccount.java
@@ -0,0 +1,36 @@
+package com.codsoft;
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+            return true;
+        } else if (amount > balance) {
+            System.out.println("Insufficient balance.");
+            return false;
+        } else {
+            System.out.println("Invalid withdrawal amount.");
+            return false;
+        }
+    }
+}
